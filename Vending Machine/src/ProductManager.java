494a862978daf31301f5314ba881e2b4499ca9cb
anthony.yarosh <anthony.yarosh@MU-C3-05.mullen.pvt.k12.co.us>
2014-10-14 14:23:41 -0600

    4th Commit txt file

--- Vending Machine/src/ProductManager.java
+++ Vending Machine/src/ProductManager.java
@@ -1,25 +1,64 @@
 //Yaroshan Code Block
 public class ProductManager
 	{
-public static void vendItem()
-	{
+	private Product p;
+	private int q;
+
+	public ProductManager(Product r, int w)
+		{
+		Product p = r;
+		int q = w;
+		}
+
+	public Product getP()
+		{
+		return p;
+		}
+
+	public int getQ()
+		{
+		return q;
+		}
+
+	public void setP(Product p)
+		{
+		this.p = p;
+		}
+
+	public void setQ(int q)
+		{
+		this.q = q;
+		}
+
+	public boolean vendItem()
+		{
+		for(Product p = 0; i < Product.p; Product.p++ )
+		
+		return false;
+
+		}
+
+	public java.lang.String getItemCode()
+		{
+		return p;
+
+		}
+
+	public Product getProduct()
+		{
+		return p;
+
+		}
+
+	public boolean isInStock()
+		{
+		return false;
+
+		}
+
+	public static void incrementItemCode()
+		{
+
+		}
 
 	}
-public static void getItemCode()
-	{
-	
-	}
-public static void getProduct()
-	{
-	
-	}
-public static void isInStock()
-	{
-	
-	}
-public static void incrementItemCode()
-	{
-	
-	}
-
-}
